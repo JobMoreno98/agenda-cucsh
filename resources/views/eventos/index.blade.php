ea9c44a020440d91dcef08f75be89c2caee10ae4
Job Moreno <jobmoreno98@gmail.com>
2025-03-11 22:13:48 -0600

    cambio lista de eventos calendario

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -1,29 +1,21 @@
-@if ($eventos->count() > 0)
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Inicia</th>
-                <th>Sede</th>
-            </tr>
-        </thead>
-        @foreach ($eventos as $item)
-            <tr>
-                <td>{{ $item->nombre }}</td>
-                <td>{{ $item->fecha_inicio }}</td>
-                <td>{{ $item->area->sedeReal }}</td>
-                @if (Auth::check())
-                    <td>
-                        <form method="POST" action="{{ route('eventos.delete', $item->id) }}" id="formularioEliminar">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="btn btn-sm btn-outline-danger">Eliminar</button>
-                        </form>
-                    </td>
-                @endif
-            </tr>
-        @endforeach
-    </table>
-@else
-    <h3>No hay eventos registrados</h3>
-@endif
+<div>
+    @forelse ($eventos as $item)
+        <div class="border border-dark rounded my-1 p-2">
+
+            Nombre: {{ $item->nombre }} <br>
+            Inicia: {{ $item->fecha_inicio . ' ' . $item->hora_inicio }} / Termina:
+            {{ $item->fecha_fin . ' ' . $item->hora_fin }}
+            <br>
+            Lugar: {{ $item->area->sedeReal }} - Edificio {{ $item->area->edificio }}
+            @if (Auth::check())
+                <form method="POST" action="{{ route('eventos.delete', $item->id) }}" id="formularioEliminar">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" class="btn btn-sm btn-outline-danger">Eliminar</button>
+                </form>
+            @endif
+        </div>
+    @empty
+        <h3>No hay eventos registrados</h3>
+    @endforelse
+</div>
